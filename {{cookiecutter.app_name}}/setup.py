0a273624df75007c84dc1bd9d986618b4afc4f43
Peter Hoffmann <ph@peter-hoffmann.com>
2016-09-27 23:12:01 +0200

    update setup.py

--- {{cookiecutter.app_name}}/setup.py
+++ {{cookiecutter.app_name}}/setup.py
@@ -10,31 +10,30 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-# get the dependencies and installs
-with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
-    all_reqs = f.read().split('\n')
 
-install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
-dependency_links = [x.strip().replace('git+', '') for x in all_reqs if 'git+' not in x]
+install_requires = []
 
-setup(
-    name='{{cookiecutter.app_name}}',
-    version=__version__,
-    description='{{cookiecutter.project_short_description}}',
-    long_description=long_description,
-    url='https://github.com/{{cookiecutter.github_username}}/{{cookiecutter.app_name}}',
-    download_url='https://github.com/{{cookiecutter.github_username}}/{{cookiecutter.app_name}}/tarball/' + __version__,
-    license='BSD',
-    classifiers=[
-      'Development Status :: 3 - Alpha',
-      'Intended Audience :: Developers',
-      'Programming Language :: Python :: 3',
-    ],
-    keywords='',
-    packages=find_packages(exclude=['docs', 'tests*']),
-    include_package_data=True,
-    author='{{cookiecutter.full_name}}',
-    install_requires=install_requires,
-    dependency_links=dependency_links,
-    author_email='{{cookiecutter.email}}'
-)
+def setup_package():
+    setup(
+        name='{{cookiecutter.app_name}}',
+        description='{{cookiecutter.project_short_description}}',
+        long_description=long_description,
+        url='https://github.com/{{cookiecutter.github_username}}/{{cookiecutter.app_name}}',
+        license='BSD',
+        classifiers=[
+          'Programming Language :: Python',
+        ],
+        keywords='',
+        packages=find_packages(exclude=['docs', 'tests*']),
+        setup_requires=['setuptools_scm'],
+        use_scm_version=True,
+        zip_safe=False,
+        include_package_data=True,
+        author='{{cookiecutter.full_name}}',
+        install_requires=install_requires,
+        dependency_links=dependency_links,
+        author_email='{{cookiecutter.email}}'
+    )
+
+if __name__ == "__main__":
+    setup_package()
